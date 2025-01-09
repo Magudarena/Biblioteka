a9b2f0e9e6656c4bcf6718005ea49eb5ac6b01ae
Madzia <bfe2f9@gmail.com>
2025-01-09 21:38:44 +0100

    #

---
+++ Biblioteka/Models/FormModel.cs
@@ -0,0 +1,13 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Biblioteka.Models
+{
+    public class FormModel
+    {
+        public Ksiazka Ksiazka { get; set; }
+        public Klient Klient { get; set; }
+
+
+
+    }
+}
